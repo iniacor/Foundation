5de9d7c5a2b07f247b33391537fd1ae9a8a57c14
ihor <evroigor86@gmail.com>
2022-06-08 20:13:17 +0300

    add .feedback

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(function () {
+  $('.feedback-slider').slick({
+    dots: true,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {},
+      },
+      {
+        breakpoint: 600,
+        settings: {},
+      },
+      {
+        breakpoint: 480,
+        settings: {},
+      },
+    ],
+  });
+});
