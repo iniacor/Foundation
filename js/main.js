4f32a826c5bf5d025dcce2810b66180d39b88e44
ihor <evroigor86@gmail.com>
2022-06-10 13:53:47 +0300

    add header 320

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
 $(function () {
+  $(window).scroll(function () {
+    var height = $(window).scrollTop();
+    if (height >= 64) {
+      $('.header').addClass('header_fix');
+      $('.headline').addClass('fix');
+    } else {
+      $('.header').removeClass('header_fix');
+      $('.headline').removeClass('fix');
+    }
+  });
+
+  $('.menu-btn').on('click', function () {
+    $('.navigation').toggleClass('navigation_active');
+  });
+
+  $('.menu-btn').on('click', function () {
+    $('.menu-btn__string').toggleClass('menu-btn__string_active');
+  });
+
+  $('.navigation__item').on('click', function () {
+    $('.navigation').toggleClass('.navigation_active');
+  });
+
   $('.feedback-slider').slick({
     dots: true,
     arrows: false,
